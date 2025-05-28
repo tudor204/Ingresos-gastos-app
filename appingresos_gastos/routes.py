4ce4b533858fc2b41e43879e0505f664f75831d8
Tudor <tudor204@hotmail.com>
2025-05-28 13:38:35 +0200

    últimos cambios

--- appingresos_gastos/routes.py
+++ appingresos_gastos/routes.py
@@ -3,4 +3,29 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    datos=[
+        {"fecha":"01/02/2025",
+        "concepto":"Ropa",
+        "monto":"-150"},
+        {"fecha":"01/03/2025",
+        "concepto":"Salario",
+        "monto":"1500"},
+        {"fecha":"15/03/2025",
+        "concepto":"Supermercado",
+        "monto":"-230"},
+        ]
+    
+    return render_template("index.html",data = datos)
+
+@app.route("/new")
+def creat():
+    return render_template("new.html")
+    
+
+
+
+"""
+{% for item in navigation %}
+        <li><a href="{{ item.href }}">{{ item.caption }}</a></li>
+    {% endfor %}
+"""
