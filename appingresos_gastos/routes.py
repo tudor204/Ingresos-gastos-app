db94624f312b169f5399c95b04b3bbbeddba7229
Tudor <tudor204@hotmail.com>
2025-05-30 13:14:17 +0200

    Agregar Validación de campos

--- appingresos_gastos/routes.py
+++ appingresos_gastos/routes.py
@@ -1,6 +1,7 @@
 from appingresos_gastos import app
-from flask import render_template,request
+from flask import render_template,request,redirect
 import csv
+from datetime import date
 
 @app.route("/")
 def index():
@@ -16,15 +17,20 @@ def index():
 @app.route("/new",methods=["GET","POST"])
 def create():
     if request.method == "POST":
-        #acceder al archivo y configurar para carar nuevo registro
-        mifichero = open("data/movimientos.csv","a",newline="")
-        #llamar al metodo writer de escritura y configuramos el formato
-        lectura = csv.writer(mifichero,delimiter=",",quotechar='"')
-        #registramos los datos recibidos
-        lectura.writerow([request.form["fecha"],request.form["concepto"],request.form["monto"]])
-        return "registro correcto"
+        errores = validateForm(request.form)
+        if errores:
+            return render_template("new.html",title="Registro",tipoAccion="Registro",tipoBoton="Guardar",error = errores,dataForm=request.form)
+        else:              
+            #acceder al archivo y configurar para carar nuevo registro
+            mifichero = open("data/movimientos.csv","a",newline="")
+            #llamar al metodo writer de escritura y configuramos el formato
+            lectura = csv.writer(mifichero,delimiter=",",quotechar='"')
+            #registramos los datos recibidos
+            lectura.writerow([request.form['fecha'],request.form['concepto'],request.form['monto']])
+            mifichero.close()
+        return redirect("/")
     else:
-        return render_template("new.html",title="Registro",tipoAccion="Registro",tipoBoton="Guardar")
+        return render_template("new.html",title="Registro",tipoAccion="Registro",tipoBoton="Guardar",dataForm={})
 
 @app.route("/update")
 def edit():
@@ -34,3 +40,15 @@ def edit():
 def remove():
     return render_template("delete.html",title="Eliminar",tipoAccion="Registro",tipoBoton="Guardar")
     
+def validateForm(datosFormulario):
+    errores=[]#lista para guardar errores
+    hoy = date.today().isoformat()#capturo laf echa de hoy
+    if datosFormulario["fecha"] > hoy:
+        errores.append("La fecha no puede ser mayor a la actual")
+    if datosFormulario["concepto"] =="":
+        errores.append("El concepto no puede ir vacío")
+    if datosFormulario["monto"] =="" or float(datosFormulario["monto"]) == 0.0:
+        errores.append("El precio debe ser distinto a 0 y de vacío")
+    return errores
+
+        
