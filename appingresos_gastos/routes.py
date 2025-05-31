491ee0f420ce96911f7f13e1b37c17b03abf7b41
Tudor <tudor204@hotmail.com>
2025-05-31 11:00:57 +0200

    Cambios pantalla update

--- appingresos_gastos/routes.py
+++ appingresos_gastos/routes.py
@@ -20,25 +20,48 @@ def create():
         errores = validateForm(request.form)
         if errores:
             return render_template("new.html",title="Registro",tipoAccion="Registro",tipoBoton="Guardar",error = errores,dataForm=request.form)
-        else:              
-            #acceder al archivo y configurar para carar nuevo registro
+        else:                         
+            
+            #generar el nuevo id en el registro
+            fichero = open("data/last_id.csv","r")
+            last_id = fichero.read()
+            if last_id == "":
+                new_id = 1
+            else:
+                new_id = int(last_id) + 1  
+            fichero.close()
+            #Guardar el nuevo last_id
+            ficheroId = open("data/last_id.csv","w")
+            ficheroId.write(str(new_id))
+            ficheroId.close()  
+
+            #acceder al archivo y configurar para cargar nuevo registro
             mifichero = open("data/movimientos.csv","a",newline="")
             #llamar al metodo writer de escritura y configuramos el formato
             lectura = csv.writer(mifichero,delimiter=",",quotechar='"')
             #registramos los datos recibidos
-            lectura.writerow([request.form['fecha'],request.form['concepto'],request.form['monto']])
+            lectura.writerow([new_id,request.form['fecha'],request.form['concepto'],request.form['monto']])
             mifichero.close()
         return redirect("/")
     else:
         return render_template("new.html",title="Registro",tipoAccion="Registro",tipoBoton="Guardar",dataForm={})
 
-@app.route("/update")
-def edit():
-    return render_template("update.html",title="Actualizar",tipoAccion="Actualizar",tipoBoton="Editar")
+@app.route("/update/<int:id>")
+def edit(id):
+    
+    return render_template("update.html",title="Actualizar",tipoAccion="Actualizar",tipoBoton="Editar",dataForm={})
 
-@app.route("/delete")
-def remove():
-    return render_template("delete.html",title="Eliminar",tipoAccion="Registro",tipoBoton="Guardar")
+@app.route("/delete/<int:id>")
+def remove(id):
+    mificheroDelete = open("data/movimientos.csv","r")
+    lectura = csv.reader(mificheroDelete,delimiter=",",quotechar='"')
+    registro_buscado=[]
+    for registros in lectura:
+        if registros[0] == str(id):
+            #aqui en cuentro el id buscado en mi registro
+            registro_buscado.append(registros)
+
+    return render_template("delete.html",title="Eliminar",tipoAccion="Registro",tipoBoton="Guardar",data = registro_buscado)
     
 def validateForm(datosFormulario):
     errores=[]#lista para guardar errores
